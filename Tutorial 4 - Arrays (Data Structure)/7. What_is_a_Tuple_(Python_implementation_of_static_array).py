45b2f8d192c47b99e40222124f0d30c0a197b464
WindJammer6 <98175995+WindJammer6@users.noreply.github.com>
2023-08-27 01:58:17 +0800

    Delete Tutorial 4 - Arrays (Data Structure) directory

--- Tutorial 4 - Arrays (Data Structure)/7. What_is_a_Tuple_(Python_implementation_of_static_array).py
+++
@@ -1,9 +0,0 @@
-#A Tuple is Python's implementation of a static array
-
-#This is how you declare it
-
-mytuple = ('tom', 'jerry', 'billy')
-
-#mytuple.insert(1, 'sam')               #If you try to modify the Tuple by inserting an element,
-                                        #the computer program will give you an error since a Tuple is
-print(mytuple[2])                       #immutable
